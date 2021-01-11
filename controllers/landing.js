cdbd2571feaf4cfd73e47c26bc4080c1258bd6f3
Juan Camilo Lopez <juclopezso@unal.edu.co>
2021-01-10 20:36:32 -0500

    post route to submit email

--- controllers/landing.js
+++ controllers/landing.js
@@ -2,3 +2,8 @@ exports.get_landing = function(req, res, next) {
   // name of the template , data
   res.render('landing', { title: 'Express' });
 }
+
+exports.submit_lead = function(req, res, next) {
+  console.log('lead email: ', req.body.lead_email);
+  res.redirect('/');
+}
