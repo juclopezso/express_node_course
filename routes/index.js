917770448a5009ac09e3205d30380a3514bfadee
Juan Camilo Lopez <juclopezso@unal.edu.co>
2021-01-10 21:29:14 -0500

    list of leads on landing

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-let landing = require('../controllers/landing');
+let landing = require("../controllers/landing");
 
 /* GET home page. */
-router.get('/', landing.get_landing);
-router.post('/', landing.submit_lead);
+router.get("/", landing.get_landing);
+router.post("/", landing.submit_lead);
+router.get("/leads", landing.show_leads);
 module.exports = router;
